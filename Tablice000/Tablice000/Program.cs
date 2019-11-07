50eda6fd9737618ed028d862060b62a5db85077d
WeronikaPodpora <56115965+WeronikaPodpora@users.noreply.github.com>
2019-11-07 14:44:41 +0100

    Sad mood

---
+++ Tablice000/Tablice000/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablice000
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Typ wartościowy
+            int a = 10;
+            int b = a;
+
+            a--;
+
+            Console.WriteLine("Zmienna a: {0}", a); //9
+            Console.WriteLine("Zmienna b: {0}", b); //11
+
+
+            //Typ referencyjny
+            int[] tabA = { 2, 2, 2 };
+            int[] tabB = tabA;
+
+            tabA[2] = 10;
+            tabB[0] = 8;
+
+            Console.WriteLine("Wyświetlenie tablicy A: ");
+            foreach (int item in tabA)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+
+
+            //Kopiowanie tablic
+            int[] tab = { 1, 2, 3, 4, 5 };
+            int[] tabCopy = new int[5];
+
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            tab.CopyTo(tabCopy, 0);
+            
+            foreach (int item in tabCopy)
+            {
+                Console.WriteLine("{0} ", item);
+            }
+
+
+            //2 sposób kopiowania tablicy
+
+            Console.WriteLine("sdf");
+
+            int[] tabCopy2 = new int[5];
+            Array.Copy(tab, tabCopy, 3);
+
+            // Array.Copy(tab, tabCopy, tab.Length);
+
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0} ", item);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ wyjątki1/wyjątki1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyjątki1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            uint number;
+            while (true) 
+            {
+                Console.Write("Wprowadź liczbę całkowitą: ");
+                string x = Console.ReadLine();
+
+                try
+                {
+                    number = uint.Parse(x);
+                    break;
+                }
+                catch (SystemException e)
+                {
+                    //Console.WriteLine("Error");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                    Console.WriteLine("Podałeś będne dane: {0}\n", x);
+                }
+            }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Prawidłowe dane: {0}", number);
+                Console.ResetColor();
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
